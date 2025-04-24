bb025fed0ad1d62d326826bb291d1ee19cda168a
Johnathan Hill <jhill115@terpmail.umd.edu>
2025-04-24 15:17:32 -0400

    Added the css for the buttons and  added the  script for the audio div

--- A2_main.js
+++ A2_main.js
@@ -1,1 +1,12 @@
 //function will go here 
+
+//Redirects to stocks page 
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('stocksButton').addEventListener('click', function() {
+        window.location.href = 'A2_Stocks.html'; 
+    });
+//Redirects to dogs page 
+    document.getElementById('dogsButton').addEventListener('click', function() {
+        window.location.href = 'A2_Dogs.html'; 
+    });
+});
